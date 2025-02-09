69eafa1beaac3283a7e1d4ee72f97fcd3a55a9ec
Rivindu Bandara <rivindub@gmail.com>
2025-02-10 10:21:35 +1100

    update

--- planning/__init__.py
+++ planning/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import os
 
-application = Flask(__name__)
+application = Flask(__name__, static_url_path='/planning/static', static_folder='static')
 application.secret_key = os.urandom(24)
+application.config['UPLOAD_FOLDER'] = '/planning/static/uploads'
 
 from planning import routes, functions
