1d86c813fa48677339f75acae5f595f10f03b357
Rivindu Bandara <rivindub@gmail.com>
2025-03-19 14:32:56 +1100

    init py

--- planning/__init__.py
+++ planning/__init__.py
@@ -4,5 +4,13 @@ import os
 application = Flask(__name__)
 application.secret_key = os.urandom(24)
 
+def patch_wsgi(app):
+    def wrapper(environ, start_response):
+        environ.setdefault('CONTENT_LENGTH', '0')
+        return app(environ, start_response)
+    return wrapper
+
+application.wsgi_app = patch_wsgi(application.wsgi_app)
+
 from planning import routes, functions
 
